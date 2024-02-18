df4457d9ee9b7299d5dcf7b3258af082efe351c9
dslayer72 <amanjp.7216@gmail.com>
2024-02-19 00:18:56 +0530

    19-Feb-2024

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Amazona</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="Products">
+          {data.products.map((product) => (
+            <div className="Product" key={product.slug}>
+              <a href={`/product/{product-slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="Product-info">
+                <a href={`/product/{product-slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
